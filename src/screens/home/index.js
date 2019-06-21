80b3db075f38d925b6dcbd40576787541e4a53af
Paulo Henrique Junior <ps.junior94@gmail.com>
2019-06-21 09:31:46 -0300

    First commit

---
+++ src/screens/home/index.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { StyleSheet, View } from 'react-native';
+
+export default class Home extends Component {
+    _sortear = () => {
+        
+    }
+
+    _addPlace = () => {
+
+    }
+
+    _removePlace = () => {
+
+    }
+
+  render() {
+    (
+        <View style={style.home}>
+            <Text>Teste</Text>
+        </View>
+    );
+  }
+}
+
+const style = StyleSheet.create({
+    home: {
+        flex: 1,
+        justifyContent: 'center'
+    }
+})
